f14c0c3f8a94ec973f97642d3847dc54e40c100f
YgorMiudo <araujo.ygor@escola.pr.gov.br>
2026-05-08 12:42:12 +0000

    trabalho realizado

---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+import chalk from 'chalk';
+
+console.log(chalk.green('Apesar de tudo que fez'), chalk.blue('a salvação ainda está a seu alcance. Você não está por aqui por acidente ou acaso, você está aqui pela graça de Deus. Você recebeu uma dádiva, agora resta saber se você aceitará ou rejeitará'));
+
