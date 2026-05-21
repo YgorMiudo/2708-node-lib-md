996c76657d9129f03f5634f9713f926b0f755c60
YgorMiudo <araujo.ygor@escola.pr.gov.br>
2026-05-21 11:00:42 +0000

    trabalho realizado

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
 import fs from 'fs';
 import chalk from 'chalk';
+  
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura [1]]: captura[2]}))
+    return resultados;
+}
 
 function trataErro(erro) {
     console.log(erro);
-    throw new Error(chalk.green(erro.code, 'não há arquivo no diretório'));
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
 //async/await
 
-async function pegaArquivo(caminhoDoArquivo){
+async function pegaArquivo(caminhoDoArquivo) {
     try {
-    const encoding = 'utf-8';
-    const texto = await fs.promises.readFile
-    (caminhoDoArquivo, encoding)
-    console.log(chalk.blue(texto));
- } catch (erro) {
+     const encoding = 'utf-8';
+     const texto = await fs.promises.readFile(caminhoDoArquivo,encoding)
+     console.log(extraiLinks(texto));
+    }  catch (erro) {
     trataErro(erro)
-}
+    }
+    
 }
 
-//promises com then()
+// promises com then()
 
-//function pegaArquivo(caminhoDoArquivo){
-    //const encoding = 'utf-8';
-   // fs.promises
-   //  .readFile(caminhoDoArquivo, encoding)
-   //  .then((texto) => console.log(chalk.blue(texto)))
-   //  .catch(trataErro)
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.promises
+//      .readFile(caminhoDoArquivo, encoding)
+//      .then((texto) => console.log(chalk.green(texto)))
+//      .catch(trataErro)
 //}
 
 //function pegaArquivo(caminhoDoArquivo) {
 //    const encoding = 'utf-8';
- //   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
- //       if(erro){
- //           trataErro(erro);
- //       }
- //       console.log(chalk.red(texto));
- //   })
- //   
+//    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => { 
+//        if (erro) {
+//            trataErro(erro);
+//        }
+//     console.log(chalk.green(texto));
+//    })
 //}
 
 pegaArquivo('./arquivos/texto.md');
-pegaArquivo('./arquivos/');
 
+
+// \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
 
 //console.log(chalk.green('Apesar de tudo que fez'), chalk.blue('a salvação ainda está a seu alcance. Você não está por aqui por acidente ou acaso, você está aqui pela graça de Deus. Você recebeu uma dádiva, agora resta saber se você aceitará ou rejeitará'));
 
