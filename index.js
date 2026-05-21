09cd56e994c1a708583780adab09c5a97248bb6e
YgorMiudo <araujo.ygor@escola.pr.gov.br>
2026-05-21 11:01:40 +0000

    trabalho realizado

--- index.js
+++ index.js
@@ -28,13 +28,7 @@ async function pegaArquivo(caminhoDoArquivo) {
 
 // promises com then()
 
-//function pegaArquivo(caminhoDoArquivo) {
-//    const encoding = 'utf-8';
-//    fs.promises
-//      .readFile(caminhoDoArquivo, encoding)
-//      .then((texto) => console.log(chalk.green(texto)))
-//      .catch(trataErro)
-//}
+
 
 //function pegaArquivo(caminhoDoArquivo) {
 //    const encoding = 'utf-8';
@@ -46,10 +40,31 @@ async function pegaArquivo(caminhoDoArquivo) {
 //    })
 //}
 
+
+
+
+
+
+
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.promises
+//      .readFile(caminhoDoArquivo, encoding)
+//      .then((texto) => console.log(chalk.green(texto)))
+//      .catch(trataErro)
+//}
+
 pegaArquivo('./arquivos/texto.md');
 
 
 // \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
+
+
+
+
+
+
 
 //console.log(chalk.green('Apesar de tudo que fez'), chalk.blue('a salvação ainda está a seu alcance. Você não está por aqui por acidente ou acaso, você está aqui pela graça de Deus. Você recebeu uma dádiva, agora resta saber se você aceitará ou rejeitará'));
 
